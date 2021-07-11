ada3f4a2f8f6bcd70a85971616a6ad5e66788e78
SweetMask13 <75247414+SweetMask13@users.noreply.github.com>
2021-07-11 22:57:05 +0300

    new version
    added little things

--- js/script.js
+++ js/script.js
@@ -92,8 +92,28 @@ $(document).ready(function(){
             data: $(this).serialize()
         }).done(function() {
             $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
             $('form').trigger('reset');
         });
         return false;
     });
+
+    //Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1400) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    })
+
+    $("a[href^='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
 });
